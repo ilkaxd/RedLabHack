d6e169fc73efc994d938f1e2cec667c768d795a9
s_litvinskaya <litvinskaya@numatech.ru>
2024-05-23 14:22:48 +0300

    icons

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,16 +1,21 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
+import Logo from "../../assets/Logo.svg?react";
+import Export from "../../assets/Export.svg?react";
+import Import from "../../assets/Import.svg?react";
 
 const Header = () => {
-    return (
-        <div className={styles.Header}>
-            <button>
-                Button 1
-            </button>
-            <button>
-                Button 2
-            </button>
-        </div>
-    );
+  return (
+    <div className={styles.Header}>
+      <h1>
+        <Logo /> 
+        Выявление аномалий во временном ряду
+      </h1>
+      <div>
+        <button><Export/>Экспорт</button>
+        <button><Import/>Импорт</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
