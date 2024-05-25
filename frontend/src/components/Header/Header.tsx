fcf4f95c98df6615579c19d8f4ff998d298e42d2
Maxim Postika <lakkiluk.m@gmail.com>
2024-05-25 16:09:34 +0300

    download fix

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -10,7 +10,19 @@ interface IProps {
 const Header = ({ options, value, onChange }: IProps) => {
   
   const onExport = () => {
-    $api.get("/export").catch((e) => console.log(e));
+    $api.get("/export", {responseType:'blob'})
+    .then(response => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'data.csv');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    })
+    .catch(error => {
+      console.error('Error downloading CSV:', error);
+    });
   };
 
   return (
